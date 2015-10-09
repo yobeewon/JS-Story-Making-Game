b34d00a4488c7556a6ecba3aec925ef084f8ee09
yobeewon <moeberry06@gmail.com>
2015-10-08 22:50:59 -0400

    Added files for word game

---
+++ jsWordGame/story.js
@@ -0,0 +1,8 @@
+var name = prompt("What is your name?");
+var age = prompt("What is your age?");
+var money = prompt("How much money do you have on you?");
+
+alert("Thank you for your input, now your results are on the way.");
+
+var message = "Hello " + name + ". I would think at the age of " + age + " you would have more than just " + money + " in your pocket.";
+document.write(message);
